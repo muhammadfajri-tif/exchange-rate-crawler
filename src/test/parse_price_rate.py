f3d61cc168e1b5b4f9523ebb36b6448d499308de
muhammadfajri-tif <149763217+muhammadfajri-tif@users.noreply.github.com>
2024-05-20 22:08:06 +0700

    test(price-parser): add test for suspicious and anomaly input

--- src/test/parse_price_rate.py
+++ src/test/parse_price_rate.py
@@ -10,8 +10,24 @@ class TestParsePriceRateMethod(unittest.TestCase):
         diff_yen = "105,00"
         self.assertEqual(parse_price_rate(diff_yen), "105.00")
 
+    def test_rate_invalid_price(self):
+        diff = "69.420,,,,"
+        self.assertIsNone(parse_price_rate(diff))
+
+    def test_rate_invalid_price_dots(self):
+        diff = "1...30"
+        self.assertIsNone(parse_price_rate(diff))
+
     def test_rate_null(self):
         diff = ""
+        self.assertIsNone(parse_price_rate(diff))
+
+    def test_rate_random_string(self):
+        diff = "this is random string"
+        self.assertIsNone(parse_price_rate(diff))
+
+    def test_rate_random_string_with_special_chars(self):
+        diff = "this is, random string."
         self.assertIsNone(parse_price_rate(diff))
 
 if __name__ == '__main__':
